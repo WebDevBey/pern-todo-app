d7229a8bd2443790cb57dcf2b33f237d66b4675c
WebDevBey <171936224+WebDevBey@users.noreply.github.com>
2025-09-03 16:43:14 +0500

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,9 @@ const PORT = process.env.PORT || 5000;
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "https://frontend-6j0r.onrender.com"
+            ));
 app.use(express.json());
 
 app.use("/todos", todoRoutes);
